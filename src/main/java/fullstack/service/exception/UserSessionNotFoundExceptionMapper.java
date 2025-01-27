decb26c3859890fa4fa81d5b46c03e13a6f2a13c
Jia Hao Christian Chen <jiahao@itsincom.it>
2025-01-27 21:42:03 +0100

    user session not found exception implementation

---
+++ src/main/java/fullstack/service/exception/UserSessionNotFoundExceptionMapper.java
@@ -0,0 +1,16 @@
+package fullstack.service.exception;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class UserSessionNotFoundExceptionMapper implements ExceptionMapper<UserSessionNotFoundException> {
+
+    @Override
+    public Response toResponse(UserSessionNotFoundException exception) {
+        return Response.status(Response.Status.UNAUTHORIZED)
+                .entity(exception.getMessage())
+                .build();
+    }
+}
