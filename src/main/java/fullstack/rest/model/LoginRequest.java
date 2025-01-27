43b4d9dde1252265226de2e442cba3bfa166b775
Jia Hao Christian Chen <jiahao@itsincom.it>
2025-01-27 21:25:44 +0100

    creating login response and login request

---
+++ src/main/java/fullstack/rest/model/LoginRequest.java
@@ -0,0 +1,28 @@
+package fullstack.rest.model;
+
+public class LoginRequest {
+    private String emailOrPhone;
+    private String password;
+
+    public String getEmailOrPhone() {
+        return emailOrPhone;
+    }
+
+    public void setEmailOrPhone(String emailOrPhone) {
+        if (emailOrPhone != null && !emailOrPhone.trim().isEmpty() && emailOrPhone.matches("\\d+")) {
+            if (!emailOrPhone.startsWith("+39")) {
+                emailOrPhone = "+39" + emailOrPhone;
+            }
+        }
+        this.emailOrPhone = emailOrPhone;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
