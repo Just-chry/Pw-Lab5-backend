42b346c9ef1761a4612e4e3dd6e4d7bb793693b4
Jia Hao Christian Chen <jiahao@itsincom.it>
2025-01-27 23:22:50 +0100

    moving class to the right folder

--- src/main/java/fullstack/rest/exception/UserSessionNotFoundExceptionMapper.java
+++ src/main/java/fullstack/rest/exception/UserSessionNotFoundExceptionMapper.java
@@ -1,5 +1,6 @@
-package fullstack.service.exception;
+package fullstack.rest.exception;
 
+import fullstack.service.exception.UserSessionNotFoundException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
