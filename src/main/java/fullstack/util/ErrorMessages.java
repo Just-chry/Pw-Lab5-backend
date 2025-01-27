065f9f1d1c6b8d73aa6b29cbfa96db91f60d962c
Jia Hao Christian Chen <jiahao@itsincom.it>
2025-01-27 23:19:45 +0100

    error messages class creation

---
+++ src/main/java/fullstack/util/ErrorMessages.java
@@ -0,0 +1,9 @@
+package fullstack.util;
+
+public class ErrorMessages {
+    public static final String NAME_REQUIRED = "Nome obbligatorio.";
+    public static final String INVALID_EMAIL = "Email non valida.";
+    public static final String INVALID_PHONE = "Numero di telefono non valido.";
+    public static final String CONTACT_REQUIRED = "È necessario fornire almeno un'email o un numero di telefono.";
+    public static final String PASSWORD_REQUIRED = "Password obbligatoria.";
+}
