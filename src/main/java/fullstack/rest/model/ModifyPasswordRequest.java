a7b5fb399b21715dbd97426ac7f277c30db538fa
Jia Hao Christian Chen <jiahao@itsincom.it>
2025-01-29 14:49:49 +0100

    modify password request creation

---
+++ src/main/java/fullstack/rest/model/ModifyPasswordRequest.java
@@ -0,0 +1,13 @@
+package fullstack.rest.model;
+
+public class ModifyPasswordRequest {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
