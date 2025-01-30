4c4af77f06889143699c87d4dbac1f653d197643
Jia Hao Christian Chen <jiahao@itsincom.it>
2025-01-30 11:26:31 +0100

    putting consumes and produces above the class

--- src/main/java/fullstack/rest/resources/SpeakerResource.java
+++ src/main/java/fullstack/rest/resources/SpeakerResource.java
@@ -14,6 +14,8 @@ import jakarta.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/speakers")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class SpeakerResource {
     private final SpeakerService speakerService;
     private final EventService eventService;
@@ -26,36 +28,30 @@ public class SpeakerResource {
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Speaker> getAllSpeakers() {
         return speakerService.getAllSpeakers();
     }
 
     @GET
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Speaker getSpeakerById(@PathParam("id") String id) {
         return speakerService.findById(id);
     }
 
     @GET
     @Path("/{id}/events")
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Event> getEventsBySpeakerId(@PathParam("id") String speakerId) {
         return eventService.getEventsBySpeakerId(speakerId);
     }
 
     @GET
     @Path("/{id}/talks")
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Talk> getSpeakersByTalkId(@PathParam("id") String speakerId) {
         return talkService.getTalksBySpeakerId(speakerId);
     }
 
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response createSpeaker(@CookieParam("sessionId") String sessionId, Speaker speaker) throws UserNotFoundException {
         try {
             Speaker savedSpeaker = speakerService.save(sessionId, speaker);
@@ -78,8 +74,7 @@ public class SpeakerResource {
 
     @PUT
     @Path("/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+
     public Response updateSpeaker(@CookieParam("sessionId") String sessionId, @PathParam("id") String id, Speaker speaker)  {
         try {
         int updated = speakerService.update(sessionId, id, speaker);
