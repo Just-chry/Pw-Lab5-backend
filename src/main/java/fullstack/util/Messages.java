2746668f926b06b875f5bf87c0018c8206341bf3
Mattia <mossalimattia@gmail.com>
2025-01-30 13:42:16 +0100

    fixed register method to have a password confirmation

--- src/main/java/fullstack/util/Messages.java
+++ src/main/java/fullstack/util/Messages.java
@@ -7,6 +7,8 @@ public class Messages {
     public static final String INVALID_PHONE = "Numero di telefono non valido.";
     public static final String CONTACT_REQUIRED = "È necessario fornire almeno un'email o un numero di telefono.";
     public static final String PASSWORD_REQUIRED = "Password obbligatoria.";
+    public static final String PASSWORD_CONFIRMATION_REQUIRED = "Conferma password obbligatoria.";
+    public static final String PASSWORD_MISMATCH = "Le password non corrispondono.";
     public static final String EMAIL_ALREADY_USED = "L'email è già in uso.";
     public static final String PHONE_ALREADY_USED = "Il numero di telefono è già in uso.";
     public static final String USER_NOT_FOUND = "Utente non trovato.";
